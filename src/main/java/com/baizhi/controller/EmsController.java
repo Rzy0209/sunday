461da2ac7c2194088a012cbea91cf520be859e47
rzy <190983994@qq.com>
2018-12-17 20:26:08 +0800

    "this is project updata"

--- src/main/java/com/baizhi/controller/EmsController.java
+++ src/main/java/com/baizhi/controller/EmsController.java
@@ -4,5 +4,7 @@ public class EmsController {
     public void m1() {
 
         System.out.println("1111111");
+        System.out.println("my is rzy");
+        System.out.println("AAAAAAAAAAA");
     }
 }
